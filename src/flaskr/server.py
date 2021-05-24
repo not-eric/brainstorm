9a4e6f4b29fb416bb7383e02e467afd373501013
jay <jayd@pdx.edu>
2021-05-23 20:57:30 -0700

    tweaks

--- src/flaskr/server.py
+++ src/flaskr/server.py
@@ -1,18 +1,8 @@
-'''
-This file is what's called by Brainstorm.js. Hopefully?
-'''
-from generate import generate as create
-from flask import Flask, render_template
+
+from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('index.html')
-
-@app.route('/generator')
-def newPiece(userName):
-    newPiece = create.newComposition(userName, 2)
-    return newPiece
-
-if __name__ == '__main__':
-  app.run(debug=True)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
