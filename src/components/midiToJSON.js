72a809abf66aac730a7a768caafd73eb9e6d717f
jay <maestro1286@gmail.com>
2021-05-27 12:34:50 -0700

    tweaks

--- src/components/midiToJSON.js
+++ src/components/midiToJSON.js
@@ -3,11 +3,11 @@ const midi = new Midi();
 
 // parse file
 function parseFile(file){
-  //read the file
+  // load a midi file in the browser
+  midi = await Midi.fromUrl("/brainstorm generator");
+  //read the file + convert
   const reader = new FileReader();
-  reader.onload = function (e) {
-    // load a midi file in the browser
-    midi = await Midi.fromUrl("/brainstorm generator");
+  reader.onload = function (midi) {
     midi = JSON.stringify(midi, undefined, 2);
   };
   reader.readAsArrayBuffer(file);
