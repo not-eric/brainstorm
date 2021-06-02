b8e43d2ab4f9b24e7f9600f52444dac63a1c6a4f
Eric Dale <not.eric.dale@gmail.com>
2021-06-02 05:10:56 -0700

    Begin addition of planet options
    Still need to style the dropdown.

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,24 +9,32 @@ export default class Form extends Component {
         this.state = {
             name: '',
             res: '',
+            planet: '',
             dataReceived: false,
+            key: 0,
         };
     }
 
     onChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value });
+        this.setState({ 
+            [event.target.name]: event.target.value,
+        });
     }
 
     onSubmit = (event) => {
         event.preventDefault();       //prevent page refresh
         
-        const { name } = this.state;     //pull from state
+        let { name } = this.state;     //pull from state
+        
+        name += this.state.planet;
 
         axios.post('http://localhost:5000/api', { name })
             .then((result) => {
                 this.setState( {res: result.data} );
                 this.setState( {dataReceived: true} );
+                this.setState({ key: Math.random() }); // re-render player
             });
+        
     }
 
     render() {
@@ -41,13 +49,26 @@ export default class Form extends Component {
                     placeholder="Name"
                     onChange={this.onChange}
                 />
+                <select name="planet" onChange={this.onChange}>
+                    <option value=""></option>
+                    <option value="mercury">Mercury</option>
+                    <option value="venus">Venus</option>
+                    <option value="mars">Mars</option>
+                    <option value="jupiter">Jupiter</option>
+                    <option value="saturn">Saturn</option>
+                    <option value="uranus">Uranus</option>
+                    <option value="neptune">Neptune</option>
+                    <option value="pluto">Pluto</option> {/* "but it's not a planet!" see next two */}
+                    <option value="sun">Sun</option>
+                    <option value="moon">Moon</option>
+                </select>
                 <button 
                     type="submit">
                         Submit
                 </button>
 
                 {dataReceived &&
-                    <Player filename={res}/>
+                    <Player key={this.state.key} filename={res}/>
                 }
                 
             </form>
