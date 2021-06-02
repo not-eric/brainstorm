8f03cf804827b39d639e854d86f022afff546e02
jared goldsmith <jgolds@pdx.edu>
2021-06-01 22:51:16 -0700

    minor cleanup home/brain

--- src/components/BrainStorm.js
+++ src/components/BrainStorm.js
@@ -7,13 +7,14 @@ import Form from "./Form.js";
 function BrainStorm() {
   return (
     <div className="gen-container">
-      <video
+      {/*<video
         className="genVideo"
         src="/videos/video2.mp4"
         autoPlay
         loop
         muted
       />
+      */}
       <h2> .generator </h2>
       <img className="modSynth" src="/images/photo2.jpg" alt="synthesizer" />
       <br></br>
