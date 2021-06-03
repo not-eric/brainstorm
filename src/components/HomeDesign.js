5c7ac7d7ac8433a93d82c3b95071134e29086d39
jared goldsmith <jgolds@pdx.edu>
2021-06-02 22:50:20 -0700

    homeRun responsiveness

--- src/components/HomeDesign.js
+++ src/components/HomeDesign.js
@@ -18,28 +18,24 @@ function HomeDesign() {
         src="/images/catsinspace.jpg"
         alt="synth cat in space"
       ></img>
-      <h2 className="headDes">
-        &nbsp; BrainStorm Music Generation<br></br>
-        {/*Generate a melody
+      <div className="topRight">
+        <h2 className="headDes">
+          &nbsp; BrainStorm Music Generation<br></br>
+          {/*Generate a melody
         based off your name and other inputs. Can be used for to inspire your
         own projects, or merely a good time.*/}
-      </h2>
-      <span className="description">
-        Generate a melody based off your
-      </span>
-      <br></br>
-      <span className="description2">
-        name and other inputs. Give it a spin
-      </span>
-      <br></br>
-      <span className="description3">
-        and inspire your next project!
-      </span>
-      <h3 className="linkGen">
-        <Link to="/generator" className="genlinks">
-          goto.generator
-        </Link>
-      </h3>
+        </h2>
+        <div className="descriptions">
+          <p className="description">Generate a melody based off your</p>
+          <p className="description2">name and other inputs. Give it a spin</p>
+          <p className="description3">and inspire your next project</p>
+        </div>
+        <h3 className="linkGen">
+          <Link to="/generator" className="genlinks">
+            goto.generator
+          </Link>
+        </h3>
+      </div>
       <h1>.home</h1>
     </div>
   );
